6979f6e576dca556f441cf2c0e988e6eb73992c1
XPaladiumyX <allegra.yann@gmail.com>
2025-10-28 15:11:55 +0100

    Main class

--- src/main/java/skyxnetwork/skyXBosses/SkyXBosses.java
+++ src/main/java/skyxnetwork/skyXBosses/SkyXBosses.java
@@ -1,17 +1,70 @@
 package skyxnetwork.skyXBosses;
 
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.plugin.java.JavaPlugin;
+import skyxnetwork.skyXBosses.managers.BossManager;
+import skyxnetwork.skyXBosses.managers.PowerManager;
+import skyxnetwork.skyXBosses.models.BossData;
 
-public final class SkyXBosses extends JavaPlugin {
+public class SkyXBosses extends JavaPlugin {
+
+    private static SkyXBosses instance;
+    private PowerManager powerManager;
+    private BossManager bossManager;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        instance = this;
+        saveDefaultConfig();
 
+        powerManager = new PowerManager(this);
+        bossManager = new BossManager(this);
+
+        getLogger().info("SkyXBosses has been enabled successfully!");
+        this.getCommand("spawnboss").setExecutor((sender, command, label, args) -> {
+            if (args.length == 0) {
+                sender.sendMessage("§cUsage: /spawnboss <bossName>");
+                return true;
+            }
+
+            String bossName = args[0].toUpperCase();
+            BossData bossData = bossManager.getBoss(bossName); // faudra créer getBoss()
+            if (bossData == null) {
+                sender.sendMessage("§cBoss not found!");
+                return true;
+            }
+
+            if (!(sender instanceof org.bukkit.entity.Player player)) {
+                sender.sendMessage("§cYou must be a player to spawn a boss.");
+                return true;
+            }
+
+            LivingEntity entity = bossData.spawnAt(player.getLocation());
+            if (entity != null) {
+                sender.sendMessage("§aBoss spawned!");
+                bossManager.getPowerExecutor().startForBoss(entity, bossData);
+            }
+
+            return true;
+        });
+
+        getCommand("skyxbosses").setExecutor(new skyxnetwork.skyXBosses.SkyXBossesCommand(this));
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        getLogger().info("SkyXBosses disabled.");
+    }
+
+    public static SkyXBosses getInstance() {
+        return instance;
+    }
+
+    public PowerManager getPowerManager() {
+        return powerManager;
+    }
+
+    public BossManager getBossManager() {
+        return bossManager;
     }
 }
